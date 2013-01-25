f4b2da46834afc548b8732a7b0d8c54bea032498
Shailesh <shailesh46@gmail.com>
2013-01-25 11:09:10 +0000

    Added the repo plugin in the repo.

--- update_zip.sh
+++ update_zip.sh
@@ -3,13 +3,12 @@
 echo "Updating "  $1  " to version "  $2 
 
 if [ $1 = "CanadaNepal" ]; then
-	echo "Packaging to a zip file"  
     zip -r repo/plugin.video.canadanepal/plugin.video.canadanepal-$2.zip plugin.video.canadanepal/
-    echo "Now copying the changelog file"
     cp plugin.video.einthusan/changelog.txt repo/plugin.video.einthusan/
 elif [ $1 = "Einthusan" ]; then
-	echo "Packaging to a zip file" 
     zip -r repo/plugin.video.einthusan/plugin.video.einthusan-$2.zip plugin.video.einthusan/
-    echo "Now copying the changelog file"
     cp plugin.video.canadanepal/changelog.txt repo/plugin.video.canadanepal/
+elif [ $1 = "Repo" ]; then
+    zip -r repo/repository.humla/repository.humla-$2.zip repository.humla/
+    cp repository.humla/changelog.txt repo/repository.humla/
 fi

4b76ee87253388751dc4edbc0e35d74bfa6dc3fd
Shailesh <shailesh46@gmail.com>
2014-09-15 22:32:26 -0400

    Modified update_zip script to run in Git Bash in Windows

--- update_zip.sh
+++ update_zip.sh
@@ -9,14 +9,14 @@ do
         if [ ! -f  $plugin/pre_release ]
         then 
             echo "Checking addon" $plugin
-            latest_version_number=`cat $plugin/addon.xml | grep "  version=" | grep -Po '(?<=")[^"]+(?=")'`
-            repo_version_number=`ls repo/$plugin/*.zip |  grep -Po '\d.\d.\d'`
+            latest_version_number=`cat $plugin/addon.xml | grep "  version=" | awk -F\" '{print $2}'`
+            repo_version_number=`ls repo/$plugin/*.zip |  awk -F\- '{print $2}' | sed 's/.zip//'`
             echo "Dev version=" $latest_version_number " VS Repo Version=" $repo_version_number
 
             if [ $latest_version_number != $repo_version_number ] ; then
                 echo "[UPDATE FOUND]: Updating to version " $latest_version_number
                 rm -rf repo/$plugin/*.zip
-                zip -rq repo/$plugin/$plugin-$latest_version_number.zip $plugin/
+		git archive --format=zip --prefix=$plugin/ HEAD:$plugin/ > repo/$plugin/$plugin-$latest_version_number.zip
                 cp $plugin/changelog.txt repo/$plugin
             fi
         fi
